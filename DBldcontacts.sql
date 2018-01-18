ff9c1821641773f6f14143dd7acc39bf3db7525b
networkseg1 <35328149+networkseg1@users.noreply.github.com>
2018-01-18 09:37:05 +0100

    Update DBldcontacts.sql

--- DBldcontacts.sql
+++ DBldcontacts.sql
@@ -5,12 +5,12 @@ GRANT  SELECT, INSERT, UPDATE ON osint.* TO 'osintuser'@localhost;
 FLUSH PRIVILEGES;
 USE osint;
 
-DROP TABLE IF EXISTS 'ldcontacts';
-CREATE TABLE 'ldcontacts' (
+DROP TABLE IF EXISTS ldcontacts;
+CREATE TABLE ldcontacts (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-'name' varchar(50) NOT NULL,  
-'name2' varchar(50) NOT NULL,
-'surname' varchar(50) NOT NULL,
-'position' varchar(500) NOT NULL,
-'company' varchar(50) NOT NULL
+name varchar(50) NOT NULL,
+name2 varchar(50) NOT NULL,
+surname varchar(50) NOT NULL,
+position varchar(500) NOT NULL,
+company varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
